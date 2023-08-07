d7dbaa09665ca6ab576f1a1632e29a676981a9fb
jelanmathewjames <72068016+jelanmathewjames@users.noreply.github.com>
2023-08-08 00:27:05 +0530

    Update all_in_one.sh

--- all_in_one.sh
+++ all_in_one.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+#prime or not
+echo "Enter a number"
+read num
+
+if [ $num -lt 2 ]; then
+    echo "$num is not a prime number."
+    exit
+fi
+
+flag=0
+for ((i=2; i<=$num/2; i++))
+do
+	if [ $(( num % i )) -eq 0 ]
+	then 
+		flag=1
+		break
+	fi
+done
+if [ $flag -eq 0 ]
+then 
+	echo "$num is prime"
+else
+	echo "$num is not prime"
+fi
 #calculator
 echo "Enter two numbers"
 read num1
